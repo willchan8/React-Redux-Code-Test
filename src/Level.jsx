080545f61aa66aede4045d9325d591439b9ec491
William Chan <wchan1992@gmail.com>
2020-07-26 21:27:05 -0700

    Complete 1: Level Component

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 //This function is responsible for rendering the level and all
 //the entities in it.
-export default function Level(props) {
+function Level(props) {
 
-  /*
+  // Convert the map from a 1-D array to a 2-D array.
+  const { mapHeight, mapWidth, map } = props;
+  const newMap = [];
+  for (let i = 0; i < mapHeight; i++) {
+    newMap.push(map.slice(i * mapWidth, (i + 1) * mapWidth ));
+  }
 
-  Your code here
-
-  */
+  // Handle conditional style/class changes for player, enemy, and wall.
+  const handleCharacterStyle = (char) => {
+    switch(char) {
+      case '@':
+        return 'player';
+      case 'e':
+        return 'enemy';
+      case '#':
+        return 'wall';
+      default:
+        return 'text';
+    }
+  }
 
   return (
     <table className="table">
       <tbody>
-      {/*
-
-      Your code here
-
-      */}
+        {newMap.map((row, rowIndex) => 
+          <tr>{row.map((square, colIndex) => 
+            <td className={handleCharacterStyle(square)} index={(rowIndex * 20) + colIndex}>{square}</td>)}
+          </tr>)}
       </tbody>
     </table>
   );
 };
+
+const mapStateToProps = (state) => ({
+  mapHeight: state.mapHeight,
+  mapWidth: state.mapWidth,
+  map: state.map,
+  player: state.player,
+});
+
+export default connect(mapStateToProps)(Level);
